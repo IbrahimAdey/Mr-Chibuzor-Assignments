8599800e3909a233ee65b10dddaad85d933576f1
IbrahimAdey <hebrospecial@yahoo.com>
2025-05-04 17:47:33 +0100

    Add files via upload

--- Kata.java
+++ Kata.java
@@ -31,5 +31,15 @@ public class Kata{
    }
      return result;
   } 
+ 
+     public static int countFactors(int number){
+     int count = 0;
+     for(int i = 1; i <= number; i++){
+      if(number % i == 0){
+         count++;
+      }
+    }
+     return count;
+}
 
 }
